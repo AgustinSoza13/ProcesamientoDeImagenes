876f9e2c9ae7dfc5c6a75cff5f6ef09d62b17039
AgustinSoza13 <agustinsoza12@gmail.com>
2023-05-05 16:48:45 -0400

    completa la Eroción, dilatación, apertura, gradiente morfologico

--- Morfologia_matematica.py
+++ Morfologia_matematica.py
@@ -1,7 +1,66 @@
-from skimage import io
-import matplotlib.pyplot as plt
-Image=io.imread("descarga.jpg")/255.0
-print("dimensiones de la imagen:\n")
-print(Image.shape)
-plt.imshow(Image)
-plt.show()
+        r, g, b = self.imagen[x,y]
+                self.imagenGris[x,y]=0.2989 * r + 0.5870 * g + 0.1140 * b
+    def VisualizarGris(self):
+        plt.imshow(self.imagenGris,cmap='gray')        
+        plt.show()
+    def VisualizarImagen(self):
+        self.imagen.show()
+
+    def getImagen(self):
+        return self.imagen
+    def getImagenGris(self):
+        return self.imagenGris
+    def getImagenErocionada(self):
+        return self.imagenErosionada
+    def getImagenDilatada(self):
+        return self.imagenDilatada
+
+
+
+    def Erocion(self,image):            
+        self.alto,self.ancho,_=self.imagen.shape
+        a=2
+        #kernel = np.ones((5,5))
+        self.imagenErosionada=np.zeros((self.alto,self.ancho))
+        for i in range(self.alto):
+            for j in range(self.ancho):
+                patch = image[i-1:i+a, j-1:j+a]
+                print(patch)
+                """
+                pivot=0
+                for t in patch:
+                    for h in t:
+                        if pivot==0:
+                            pivot=h
+                        if pivot>h:
+                            pivot=h 
+                """
+                #self.imagenErosionada[i, j]=pivot                   
+                #imagenErosionada[i, j] = min(min(fila) for fila in patch)#erosio
+
+        #plt.imshow(self.imagenErosionada,cmap="gray")
+        #plt.show()
+
+    def Dilatacion(self,image):
+        a=2
+        #kernel = np.ones((5,5))
+        self.imagenDilatada=np.zeros((self.alto,self.ancho))
+        for i in range(self.alto):
+            for j in range(self.ancho):
+                patch = image[i-1:i+a, j-1:j+a]
+                pivot=0
+                for t in patch:
+                    for h in t:
+                        if pivot==0:
+                            pivot=h
+                        if pivot<h:
+                            pivot=h 
+                self.imagenDilatada[i, j]=pivot                   
+                #imagenErosionada[i, j] = min(min(fila) for fila in patch)#erosio
+
+        plt.imshow(self.imagenDilatada,cmap="gray")
+        plt.show()
+
+
+    def TopHat(self):
+        pass
--- unidad_1/Morfologia_poo.py
+++ unidad_1/Morfologia_poo.py
@@ -1,41 +1,127 @@
 
 import matplotlib.pyplot as plt
+from skimage import io
 from PIL import Image
 import numpy as np
 import matplotlib.image as mpimg
+import cv2
 class Imagen:
     def __init__(self,ruta):
         self.ruta=ruta
     def CargarImagen(self):
-        self.imagen=Image.open(self.ruta)
-    def EscalaDeGrises(self):
-        img = mpimg.imread('descarga.jpg')
-        alto,ancho,_=img.shape
+        self.imagen=cv2.imread(self.ruta)
+    def convertir_a_grises(self):
+        self.imagen = cv2.cvtColor(self.imagen, cv2.COLOR_BGR2GRAY)
 
-        #self.ImagenGris=Image.new("L",(self.ancho,self.alto))#retorna imagen en escala de grises
-        self.imagenGris=np.zeros((alto,ancho))
-        for x in range(alto):
-            for y in range(ancho):
-                r, g, b = img[x,y]
+    def EscalaDeGrises(self):
+        
+        self.imagenGris=np.zeros((self.alto,self.ancho))
+        for x in range(self.alto):
+            for y in range(self.ancho):
+                r, g, b = self.imagen[x,y]
                 self.imagenGris[x,y]=0.2989 * r + 0.5870 * g + 0.1140 * b
+    def VisualizarGris(self):
         plt.imshow(self.imagenGris,cmap='gray')        
         plt.show()
-        #return self.imgenGris
-    
     def VisualizarImagen(self):
         plt.imshow(self.imagen)
-        plt.show() 
-    def VisualizarGris(self):
-        plt.imshow(self.imagenGris)
         plt.show()
+        
+    def getImagen(self):
+        return self.imagen
+    def getImagenGris(self):
+        return self.imagenGris
+    def getImagenErocionada(self):
+        return self.imagenErosionada
+    def getImagenDilatada(self):
+        return self.imagenDilatada
+    def getImagenApetura(self):
+        return self.imagenApertura
 
+    def Erocion(self,image):            
+        x,y,=image.shape
+        a=2
+        #kernel = np.ones((5,5))
+        self.imagenErosionada=np.zeros((x,y))
+        for i in range(x):
+            for j in range(y):
+                patch = image[i-1:i+a, j-1:j+a]
+                pivot=0
+                for t in patch:
+                    for h in t:
+                        if pivot==0:
+                            pivot=h
+                        if pivot>h:
+                            pivot=h 
+                self.imagenErosionada[i, j]=pivot   
+        plt.imshow(self.imagenErosionada)   
+        plt.title("Eroción")             
+
+    def Dilatacion(self,image):
+        x,y=image.shape
+        a=2
+        #kernel = np.ones((5,5))
+        self.imagenDilatada=np.zeros((x,y))
+        for i in range(x):
+            for j in range(y):
+                patch = image[i-1:i+a, j-1:j+a]
+                pivot=0
+                for t in patch:
+                    for h in t:
+                        if pivot==0:
+                            pivot=h
+                        if pivot>h:
+                            pivot=h
+                self.imagenDilatada[i, j]=pivot                   
+                
+
+                 
+        
+    def Apertura(self,a):
+        
+        pass
+
+    def TopHat(self):
+        pass
+# originalgriss-
 if __name__ == '__main__':
-    lena=Imagen("descarga.jpg")
-    lena.CargarImagen()
-    print()
-    lena.EscalaDeGrises()
-    lena.VisualizarGris()
+
+    camara=Imagen("uno.png")
+    camara.CargarImagen()
+    camara.convertir_a_grises()
+    camara.Erocion(camara.getImagen())
+    camara.Dilatacion(camara.getImagen())
+    #camara.Apertura(camara.Dilatacion())
+    #plt.imshow(camara.getImagenApetura,cmap="gray")
+    #plt.show()
+    
+    fig,axs=plt.subplots(3,3,figsize=(8,8))#
+    axs[0,0].imshow(camara.getImagen(),cmap="gray")
+    axs[0,1].imshow(camara.getImagenErocionada(),cmap="gray")
+    axs[1,0].imshow(camara.getImagenDilatada(),cmap="gray")
+    axs[1,1].imshow(camara.getImagenDilatada()-camara.getImagen(),cmap="gray")
+    plt.show()
     
 
+    plt.imshow(camara.getImagenDilatada()-camara.getImagen(),cmap="gray")
+    plt.title("gradiente morfologico")
+    #camara.Erocion(camara.getImagen())
+    #plt.imshow(camara.getImagenDilatada()-camara.getImagen())#gradiente morfologico
+    plt.show()
+    #plt.imshow(camara.getImagenErocionada()-camara.getImagen(),cmap="gray")
+    #plt.show()
+    #-----
+    plt.imshow(camara.getImagen()-camara.getImagenDilatada(),cmap="gray")
+    plt.title("uno")
+    plt.show()
+    plt.imshow(camara.getImagen()-camara.getImagenErocionada(),cmap="gray")
+    plt.title("dos")
+    plt.show()
 
+    camara.Dilatacion(camara.getImagenErocionada())
+    plt.imshow(camara.getImagen()-camara.getImagenDilatada(),cmap="gray")
+    plt.title("final")
+    plt.show()
+
+#diferencia finita
 
